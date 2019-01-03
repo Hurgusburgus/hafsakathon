e9c3099e52ca51fbf4a086fbfa0c893890a553ea
Shikli <f.chikli@gmail.com>
2019-01-03 12:19:49 +0200

    added users generator and adding function

--- webAPI/generate_dummy_sql.py
+++ webAPI/generate_dummy_sql.py
@@ -1,7 +1,85 @@
-def generate_useres():
+import numpy as np
+import json
+import sqlite3
+
+
+DATABASE = 'recess.db'
+def generate_useres(n):
     firstnames = ["Oren", "Gabe", "Shay", "Smadar", "Guy", "Roman", "Roee",
                   "Matan", "Yotam", "Shir", "Michal", "Yigal", "Neta", "Maya"]
     lastnames =["Chikli", "Rimon", "Danon", "Weinstein", "strauss", "Haim",
                 "Tshova", "Amari", "nestrov", "tianov", "Cohen", "Braunstein"
                 "Bar", "pier"]
-    birth_dates = [""]
+    birth_dates = ["1989-05-22", "2000-04-03", "1982-07-30","1986-10-24",
+                   "1994-08-12", "1997-08-15", "2001-06-18", "1996-02-18",
+                   "1995-07-15", "1987-08-19","1996-05-01","1983-01-01",
+                   "2001-02-05"]
+    user_names = ["Superman","Batman","Bob","Batgirl", "Bigbuy", "Aquaman",
+                   "Neo", "Trinity", "Ninja", "Dva"]
+
+    sex_op = ["M","F"]
+    cities =["Tel-Aviv", "Netanya", "Hafia", "Jaffa", "Jerusalem"]
+    pass_ = "password"
+    reg_dates = ["2018-01-03","2018-01-02","2018-01-04"]
+    email_appen = "@gmail.com"
+    phone_start = ["050", "052", "053", "054" ,"055"]
+
+    users = []
+    for i in range(n):
+        curr_user = {}
+        curr_user['username'] = np.random.choice(user_names) + \
+                                str(np.random.randint(10))
+        curr_user['firstname'] = np.random.choice(firstnames)
+        curr_user['lastname'] = np.random.choice(lastnames)
+        curr_user['birth'] = np.random.choice(birth_dates)
+        curr_user['sex'] = np.random.choice(sex_op)
+        curr_user['city'] = np.random.choice(cities)
+        curr_user['phone'] = np.random.choice(phone_start) + '-'\
+                            + str(np.random.randint(100, 1000)) + '-'\
+                            + str(np.random.randint(1000, 10000))
+        curr_user['email'] = curr_user['username'] + email_appen
+        curr_user['pass'] = pass_ + str(np.random.randint(100))
+        curr_user['reg_date'] = np.random.choice(reg_dates)
+        users.append(curr_user)
+
+    return users
+
+
+def useres_insert(given_input):
+    with sqlite3.connect(DATABASE)as con:
+        cur = con.cursor()
+
+        insertion_querry =\
+        """INSERT INTO users (username,firstname, lastname,birth,sex,city,
+                              phone,email,pass,reg_date)
+                       VALUES ('{username}', '{firstname}' ,'{lastname}', 
+                                '{birth}', '{sex}', '{city}', '{phone}', 
+                                '{email}', '{pass_}','{reg_date}') """
+        for row in given_input:
+            query = insertion_querry.format(username=row['username'],
+                                            firstname=row['username'],
+                                            lastname=row['lastname'],
+                                            birth=row['birth'],
+                                            sex=row['sex'],
+                                            city=row['city'],
+                                            phone=row['phone'],
+                                            email=row['email'],
+                                            pass_=row['pass'],
+                                            reg_date=row['reg_date'])
+
+            cur.execute(query)
+            con.commit()
+
+        cur.execute("select * from users")
+        for row in cur.fetchall():
+            print(row)
+
+
+
+def main():
+    users = generate_useres(5)
+    useres_insert(users)
+
+if __name__ == "__main__":
+    main()
+
--- webAPI/sqlite _createdb.py
+++ webAPI/sqlite _createdb.py
@@ -1,6 +1,50 @@
 import sqlite3
 
 
+with sqlite3.connect('recess.db')as con:
+    cur = con.cursor()
+    # crate table
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    username text NOT NULL,
+    firstname text,
+    lastname text,
+    birth DATE,
+    sex text DEFAULT 'N',
+    city text NOT NULL,
+    phone text,
+    email text NOT NULL,
+    pass text NOT NULL,
+    description text,
+    reg_date TIMESTAMP
+    );
+    """)
+
+
+    con.commit()
+    cur.close()
+
+
+with sqlite3.connect('recess.db')as con:
+    cur = con.cursor()
+    # crate table
+    cur.execute("""CREATE TABLE IF NOT EXISTS games (
+    id_game integer PRIMARY KEY AUTOINCREMENT,
+    game_type text,
+    game_name text,
+    game_day DATE not null,
+    start_time text NOT NULL,
+    location text,
+    min_players integer NOT NULL DEFAULT 2,
+    max_players integers,
+    num_teams integer DEFAULT 0
+    );
+    """)
+
+
+    con.commit()
+    cur.close()
+
 def create_users_table():
     with sqlite3.connect('recess.db')as con:
         cur = con.cursor()
@@ -56,40 +100,63 @@ def useres_insert():
                                 02/01/2019);
                     """)
 
-    cur.execute("""INSERT INTO users (
-    username,
-    firstname,
-    lastname,
-    birth,
-    sex,
-    city,
-    phone,
-    email,
-    pass,
-    description,
-    reg_date)
-
-    VALUES (
-    'smadar',
-    'smadar',
-    'danon',
-    05/05/1994,
-    'female',
-    'tel aviv',
-    05555555,
-    'smadar@gmail.com',
-    123456,
-    'hello',
-    02/01/2019
-    );
-    """)
-
-
-    con.commit()
-    cur.close()
+        cur.execute("""INSERT INTO users (
+        username,
+        firstname,
+        lastname,
+        birth,
+        sex,
+        city,
+        phone,
+        email,
+        pass,
+        description,
+        reg_date)
+    
+        VALUES (
+        'smadar',
+        'smadar',
+        'danon',
+        05/05/1994,
+        'female',
+        'tel aviv',
+        05555555,
+        'smadar@gmail.com',
+        123456,
+        'hello',
+        02/01/2019
+        );
+        """)
 
 
-    with sqlite3.connect('recess.db')as con:
+        # crate table
+        cur.execute("""INSERT INTO users (
+        username,
+        firstname,
+        lastname,
+        birth,
+        sex,
+        city,
+        phone,
+        email,
+        pass)
+    
+        VALUES (
+        'barb',
+        'barbara',
+        'cohen',
+        date('1994-07-07'),
+        'female',
+        'tel aviv',
+        054466771,
+        'barb@gmail.com',
+        123456
+        );
+        """)
+
+
+        con.commit()
+
         cur = con.cursor()
         cur.execute("""SELECT* FROM users""")
         rows = cur.fetchall()
@@ -97,6 +164,21 @@ def useres_insert():
         print(rows)
 
         cur.close()
+
+
+
+
+with sqlite3.connect('recess.db')as con:
+    cur = con.cursor()
+    # crate table
+
+    con.commit()
+    cur.close()
+#
+
+
+
+
 
 def insert_games():
     with sqlite3.connect('recess.db')as con:
@@ -159,12 +241,23 @@ def insert_games():
         );
         """)
 
-        cur.execute("""SELECT* FROM games""")
-        rows = cur.fetchall()
-        #     print(rows)
-        print(rows)
+        cur.execute("""INSERT INTO games (
+        game_type,
+        game_name,
+        game_day,
+        start_time,
+        location,
+        max_players)
 
-        con.commit()
+        VALUES (
+        'soccer',
+        'team',
+        date('1994-07-07'),
+        'bla',
+        'jerusalem',
+        5
+        );
+        """)
 
 
 
