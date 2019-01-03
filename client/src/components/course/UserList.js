b1e6bf6c8e073556ed3b37c03ac779e816f8ce89
GitGudGabey <gxrimn@gmail.com>
2019-01-03 15:43:09 +0200

    view users

--- client/src/components/course/UserList.js
+++ client/src/components/course/UserList.js
@@ -1,29 +1,42 @@
 import React, {PropTypes} from 'react';
 import GameListRow from './GameListRow';
 
-const UserList = ({users, joinGameFunc}) => {
-  console.log(games)
+const UserList = ({users}) => {
+  console.log(users)
   return (
     <table className="table">
       <thead>
       <tr>
         <th>Username</th>
-        <th></th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Birth date</th>
+        <th>Sex</th> 
+        <th>City</th> 
+        <th>Phone</th>
+        <th>Email</th>
       </tr>
       </thead>
       <tbody>
       {users.map(user =>
         <tr>
             <td>{user.username}</td>
+            <td>{user.firstname}</td>
+            <td>{user.lastname}</td>
+            <td>{user.birth}</td>
+            <td>{user.sex}</td> 
+            <td>{user.city}</td> 
+            <td>{user.phone}</td>
+            <td>{user.email}</td>
         </tr>
-      )};
+      )}
       </tbody>
     </table>
   );
 };
 
-GameList.propTypes = {
-  games: PropTypes.array.isRequired
+UserList.propTypes = {
+  users: PropTypes.array.isRequired
 };
 
-export default GameList;
+export default UserList;
